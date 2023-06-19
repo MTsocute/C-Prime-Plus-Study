c03c03a664d4a1252fffec4771dc57cc5c03a22c
Momo <75737534+MTsocute@users.noreply.github.com>
2023-06-19 17:12:01 +0800

    整理了排版，清爽点

--- Chapter14/结构体.h
+++ Chapter14/结构体.h
@@ -4,8 +4,10 @@
 
 #ifndef MAIN_CPP_结构体_H
 #define MAIN_CPP_结构体_H
+
 #include <cstdio>
 #include <cstdlib>
+
 #define MAXTITL 41
 #define MAXAUTL 31
 #define LEN 20
@@ -29,22 +31,22 @@ struct book2 {
 /* 初始化一个结构体变量 */
 
 // 全部初始化，按顺序
-struct book nice {
-    "babablaalala",
-    "Momo",
-    200000000.123123123
+struct book nice{
+        "babablaalala",
+        "Momo",
+        200000000.123123123
 };
 
 // 初始化指定部分
-struct book nice2 {
+struct book nice2{
         .value = 200000000.123123123
 };
 
 // 初始化全部，不按顺序
-struct book nice3 {
-    .author = "momo",
-    .title = "Me and my friend",
-    .value = 250.12
+struct book nice3{
+        .author = "momo",
+        .title = "Me and my friend",
+        .value = 250.12
 };
 
 
@@ -53,7 +55,6 @@ struct book nice3 {
  * 数组下的每一个变量都是 struct book 类型
  * */
 struct book library[10];
-
 
 struct names {
     char first[LEN];
@@ -70,31 +71,33 @@ struct guy {
 
 
 // 结构体嵌套匿名：不需要在外名声明一个结构体，里面再见一个变量了
-struct person
-{
+struct person {
     int id;
     // 匿名结构
-    struct {char first[20]; char last[20];};
+    struct {
+        char first[20];
+        char last[20];
+    };
 };
-
 
 
 /* 使用结构体指针 */
 void use_of_StructPoint() {
     // 结构体数组
-    struct guy fellow[2] = {
-            { { "Ewen", "Villard" },
-                    "grilled salmon",
-                    "personality coach",
-                    68112.00
-            },
-            { { "Rodney", "Swillbelly" },
-                    "tripe",
-                    "tabloid editor",
-                    432400.00
-            }
-    };
-    struct guy * him;        /* 这是一个指向结构的指针 */
+    struct guy fellow[2] =
+            {
+                    {{"Ewen",   "Villard"},
+                            "grilled salmon",
+                            "personality coach",
+                            68112.00
+                    },
+                    {{"Rodney", "Swillbelly"},
+                            "tripe",
+                            "tabloid editor",
+                            432400.00
+                    }
+            };
+    struct guy *him;        /* 这是一个指向结构的指针 */
 
     printf("address #1: %p #2: %p\n", &fellow[0], &fellow[1]);
     // 注意结构体不是数组，变量名存储的不是指向地址的指针，还是需要取地址
